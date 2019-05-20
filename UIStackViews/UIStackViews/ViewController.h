3d706dd8590961d9d9e73043be82fb048fb18372
Zihua Liu <liuzihua0911@gmail.com>
2019-05-20 15:58:57 -0400

    Update UIStackViewsUITests project

---
+++ UIStackViews/UIStackViews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIStackViews
+//
+//  Created by Zihua Liu on 5/20/19.
+//  Copyright © 2019 Zihua Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIStackViews/UIStackViews/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  UIStackViews
+//
+//  Created by Zihua Liu on 5/20/19.
+//  Copyright © 2019 Zihua Liu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
