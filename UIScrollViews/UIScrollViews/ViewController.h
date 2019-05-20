5f31f97ae5e305060f547d19d5e237d325b20e2b
Zihua Liu <liuzihua0911@gmail.com>
2019-05-20 15:04:53 -0400

    Update UIScrollViews project

---
+++ UIScrollViews/UIScrollViews/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  UIScrollViews
+//
+//  Created by Zihua Liu on 5/20/19.
+//  Copyright © 2019 Zihua Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@end
+
---
+++ UIScrollViews/UIScrollViews/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIScrollViews
+//
+//  Created by Zihua Liu on 5/20/19.
+//  Copyright © 2019 Zihua Liu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [self.scrollView setContentSize:CGSizeMake(0, 1500)];
+}
+
+
+@end
