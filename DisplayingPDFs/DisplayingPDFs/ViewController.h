cbc755bceb4a5582015d3f81c679e370b54ff2c3
Zihua Liu <liuzihua0911@gmail.com>
2019-05-23 00:24:36 -0400

    Update DisplayingPDFs project

---
+++ DisplayingPDFs/DisplayingPDFs/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  DisplayingPDFs
+//
+//  Created by Zihua Liu on 5/23/19.
+//  Copyright © 2019 Zihua Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet WKWebView *webView;
+
+@end
+
---
+++ DisplayingPDFs/DisplayingPDFs/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DisplayingPDFs
+//
+//  Created by Zihua Liu on 5/23/19.
+//  Copyright © 2019 Zihua Liu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"Cats" ofType:@"pdf"];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+}
+
+
+@end
